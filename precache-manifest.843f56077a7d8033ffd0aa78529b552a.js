b36ca4b72b1dd24e47c8f98127359354c017c087
Vishwas Navada <navadavishwas@hotmail.com>
2019-08-25 17:48:20 +0530

    Updates

---
+++ precache-manifest.843f56077a7d8033ffd0aa78529b552a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54232094aaa33a0fbdd7185e0ef0f80f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "724c14494019e37e0805",
+    "url": "/resume/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "08015e2bc7fbf79f7091",
+    "url": "/resume/static/css/main.301c9041.chunk.css"
+  },
+  {
+    "revision": "724c14494019e37e0805",
+    "url": "/resume/static/js/2.b4355051.chunk.js"
+  },
+  {
+    "revision": "08015e2bc7fbf79f7091",
+    "url": "/resume/static/js/main.1baf77ab.chunk.js"
+  },
+  {
+    "revision": "857696afbb2a4f067ea2",
+    "url": "/resume/static/js/runtime~main.cbb32322.js"
+  }
+]);
